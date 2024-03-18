cc99753e37aa801d8ba8a6f3e86198d735b89904
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-03-18 16:57:17 +0100

    44

--- kamaji.nix
+++ kamaji.nix
@@ -170,12 +170,36 @@ in
     };
   };
 
-  #security.sudo = {
-    #pwfeedback = true;
-    #timestamp_timeout = 30;
-    # ...other sudo options...
-  #};
+  services.openssh.enable = true;
 
+  ## Define a systemd service for SSH agent
+  systemd.user.services.ssh-agent = {
+    enable = true;
+    description = "SSH agent";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.openssh}/bin/ssh-agent -D -a $HOME/.ssh/agent-socket";
+      Restart = "always";
+      RestartSec = "10";
+      KillMode = "process";
+    };
+  };
+
+  # Automatically add your SSH key to the agent
+  environment.sessionVariables.SSH_AUTH_SOCK = "${builtins.getEnv "HOME"}/.ssh/agent-socket";
+
+  # Define a shell script to add SSH key
+  environment.etc."profile.d/ssh-add.sh".text = ''
+    if [ -n "$SSH_AUTH_SOCK" ] && [ -z "$(ssh-add -l)" ]; then
+      ssh-add ~/.ssh/github
+    fi
+  '';
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -185,21 +209,6 @@ in
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?

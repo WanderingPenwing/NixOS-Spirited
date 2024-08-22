299038e1ca299e730139fc8f709b8c4cc6de6c31
Penwing <wandering.penwing@gmail.com>
2024-08-22 16:52:54 +0200

    NixOS rebuild: try

--- system/boiler.nix
+++ system/boiler.nix
@@ -14,6 +14,11 @@ in {
     ./boiler-hardware.nix
   ];
 
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
   networking.hostName = hostname;
 
   services.openssh.enable = true;

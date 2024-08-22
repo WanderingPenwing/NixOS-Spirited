148ce5f6676fecdd242c0ae44699cb035d223a51
Penwing <wandering.penwing@gmail.com>
2024-08-22 17:43:19 +0200

    NixOS rebuild: try

--- system/boiler.nix
+++ system/boiler.nix
@@ -22,7 +22,14 @@ in {
   networking.hostName = hostname;
 
   services.openssh.enable = true;
-  services.openssh.settings.PasswordAuthentication = true;
+  services.openssh.settings.PasswordAuthentication = false;
+
+  users.users.penwing = {
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN/SQbXjL6O2zjKFdybiMLu7Imc10IGrTMUnRtIxf0jJ nicolas.pinson31@gmail.com"
+        ];
+  };
+
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -30,9 +37,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = (with pkgs; [
-  
-    # CLIs
-    bc # calculator
+    fastfetch
   ]);
 
   # Before changing this value read the documentation for this option

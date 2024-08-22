333ae7a21ad15364a2d7e53607cb10cd109c98e3
penwing-bathhouse <nicolas.pinson31@gmail.com>
2024-08-22 20:00:09 +0200

    NixOS rebuild: try

--- system/bathhouse.nix
+++ system/bathhouse.nix
@@ -39,8 +39,6 @@ in {
 
   services.nginx.enable = true;
 
-  services.docker.enable = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = (with pkgs; [

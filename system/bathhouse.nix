d2aa2557a11ab70e2cf4c343989fc47d4f2ae8da
penwing-bathhouse <nicolas.pinson31@gmail.com>
2024-09-03 15:01:50 +0200

    NixOS rebuild: try

--- system/bathhouse.nix
+++ system/bathhouse.nix
@@ -82,7 +82,7 @@ in {
       };
       
       "paper.penwing.org" = {
-        locations."/".proxyPass = "http://192.168.1.42:1180";     
+        locations."/".proxyPass = "http://192.168.1.42:80";     
       };
 
       "key.penwing.org" = {

e0ee6d5934e78bedb096c371328efe571fa6bbfe
WanderingPenwing <wandering.penwingl@2025.icam.fr>
2024-11-08 16:55:39 +0100

    NixOS rebuild: try

--- home/kamaji-home.nix
+++ home/kamaji-home.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 {
-
   programs = {
   	git = {
       enable = true;
@@ -12,94 +11,7 @@
       };
     };
 
-    starship = {
-      enable = true;
-      settings = {
-        character = {
-          success_symbol = "[>](bold green)";
-          error_symbol = "[x](bold red)";
-          vimcmd_symbol = "[<](bold green)";
-        };
-        git_commit = {
-          tag_symbol = " tag ";
-        };
-        git_status = {
-          ahead = ">";
-          behind = "<";
-          diverged = "<>";
-          renamed = "r";
-          deleted = "x";
-        };
-        aws = { symbol = "aws "; };
-        azure = { symbol = "az "; };
-        bun = { symbol = "bun "; };
-        c = { symbol = "C "; };
-        cobol = { symbol = "cobol "; };
-        conda = { symbol = "conda "; };
-        crystal = { symbol = "cr "; };
-        cmake = { symbol = "cmake "; };
-        daml = { symbol = "daml "; };
-        dart = { symbol = "dart "; };
-        deno = { symbol = "deno "; };
-        dotnet = { symbol = ".NET "; };
-        directory = {
-          read_only = " ro";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-          substitutions = {
-            docs = " 🕮  ";
-            dl = "   ";
-            pics = "   ";
-            nixos = " ❆ ";
-          };
-        };
-        docker_context = { symbol = " "; };
-        git_branch = { symbol = " "; };
-        lua = { symbol = "lua "; };
-        nodejs = { symbol = " "; };
-        memory_usage = { symbol = "memory "; };
-        nix_shell = { symbol = "nix "; };
-        os = { disabled = true; };
-        package = { symbol = " "; };
-        php = { symbol = "php "; };
-        python = { symbol = "py "; };
-        rust = { symbol = " "; };
-        scala = { symbol = "scala "; };
-        spack = { symbol = "spack "; };
-        solidity = { symbol = "solidity "; };
-        status = { symbol = "[x](bold red) "; };
-        sudo = { symbol = "sudo "; };
-        swift = { symbol = "swift "; };
-        typst = { symbol = "typst "; };
-        terraform = { symbol = "terraform "; };
-        zig = { symbol = "zig "; };
-      };
-    };
     
-    nushell = {
-      enable = true;
-      extraConfig = ''
-       $env.config = {
-          show_banner: false,
-          completions: {
-            case_sensitive: false,
-            quick: true,
-            partial: true,
-            algorithm: "fuzzy",
-            external: {
-              enable: true,
-              max_results: 100,
-            }
-          }
-        }
-        $env.PATH = ($env.PATH | split row (char esep) | prepend /home/penwing/.apps | append /usr/bin/env)
-      '';
-      shellAliases = {
-        m = "micro";
-        y = "yazi";
-        rebuild = "~/nixos/scripts/rebuild.sh";
-      };
-    };
      
     #carapace.enable = true;
     #carapace.enableNushellIntegration = true;

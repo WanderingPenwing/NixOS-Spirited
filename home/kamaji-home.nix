a60dc7be5ee9481dd9fddad9864a56a47b2dfa46
WanderingPenwing <wandering.penwingl@2025.icam.fr>
2024-11-08 16:35:26 +0100

    NixOS rebuild: try

--- home/kamaji-home.nix
+++ home/kamaji-home.nix
@@ -1,30 +1,77 @@
 { config, pkgs, ... }:
 
 {
-#   home.packages = with pkgs; [
-#     dconf
-#   ];
-# 
-#   dconf = {
-#     enable = true;
-#     settings = {
-#       "org/gnome/desktop/interface" = {
-#         color-scheme = "prefer-dark";
-#       };
-#     };
-#   };
   
   programs.git = {
     enable = true;
-
-    # Git global user settings
     userName = "WanderingPenwing";       # Replace with your actual name
     userEmail = "wandering.penwingl@2025.icam.fr";  # Replace with your actual email
-
-    # Git configuration options
     extraConfig = {
-      # Set default branch name when initializing a repository
       init.defaultBranch = "main";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      character = {
+        success_symbol = "[>](bold green)";
+        error_symbol = "[x](bold red)";
+        vimcmd_symbol = "[<](bold green)";
+      };
+      git_commit = {
+        tag_symbol = " tag ";
+      };
+      git_status = {
+        ahead = ">";
+        behind = "<";
+        diverged = "<>";
+        renamed = "r";
+        deleted = "x";
+      };
+      aws = { symbol = "aws "; };
+      azure = { symbol = "az "; };
+      bun = { symbol = "bun "; };
+      c = { symbol = "C "; };
+      cobol = { symbol = "cobol "; };
+      conda = { symbol = "conda "; };
+      crystal = { symbol = "cr "; };
+      cmake = { symbol = "cmake "; };
+      daml = { symbol = "daml "; };
+      dart = { symbol = "dart "; };
+      deno = { symbol = "deno "; };
+      dotnet = { symbol = ".NET "; };
+      directory = {
+        read_only = " ro";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+        substitutions = {
+          Documents = " 🕮  ";
+          Downloads = "   ";
+          Pictures = "   ";
+          nixos = " ❆ ";
+        };
+      };
+      docker_context = { symbol = " "; };
+      git_branch = { symbol = " "; };
+      lua = { symbol = "lua "; };
+      nodejs = { symbol = " "; };
+      memory_usage = { symbol = "memory "; };
+      nix_shell = { symbol = "nix "; };
+      os = { disabled = true; };
+      package = { symbol = " "; };
+      php = { symbol = "php "; };
+      python = { symbol = "py "; };
+      rust = { symbol = " "; };
+      scala = { symbol = "scala "; };
+      spack = { symbol = "spack "; };
+      solidity = { symbol = "solidity "; };
+      status = { symbol = "[x](bold red) "; };
+      sudo = { symbol = "sudo "; };
+      swift = { symbol = "swift "; };
+      typst = { symbol = "typst "; };
+      terraform = { symbol = "terraform "; };
+      zig = { symbol = "zig "; };
     };
   };
 

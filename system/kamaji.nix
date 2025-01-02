3c2aea50b2be0ae235f0abda0c72f4537f903a3c
WanderingPenwing <wandering.penwingl@2025.icam.fr>
2025-01-02 10:43:53 +0100

    NixOS rebuild: try

--- system/kamaji.nix
+++ system/kamaji.nix
@@ -128,7 +128,6 @@ in {
   programs.noisetorch.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {

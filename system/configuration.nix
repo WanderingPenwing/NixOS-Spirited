bfd2eb61bc938a176703e00bd4d26c294ddc8713
WanderingPenwing <wandering.penwing@gmail.com>
2025-01-03 22:58:17 +0100

    NixOS rebuild: try

--- system/configuration.nix
+++ system/configuration.nix
@@ -66,7 +66,6 @@
     EDITOR = "micro";
     YAZI_CONFIG_HOME = "$HOME/nixos/apps/yazi";
     ENV = "$HOME/nixos/scripts/mkshrc";
-    NIX_BUILD_SHELL = "${pkgs.mksh}/bin/mksh";
   };
 
   fonts.packages = with pkgs; [

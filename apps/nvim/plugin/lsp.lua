a8c1c1b1c949e00f1a599bd164f85fed7f921a86
WanderingPenwing <wandering.penwing@gmail.com>
2025-06-02 23:58:00 +0200

    NixOS rebuild: try

--- apps/nvim/plugin/lsp.lua
+++ apps/nvim/plugin/lsp.lua
@@ -1,22 +1,37 @@
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 local on_attach = function(_, bufnr)
 
-	local bufmap = function(keys, func)
-		vim.keymap.set('n', keys, func, { buffer = bufnr })
+	local bufmap = function(keys, func, desc)
+		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 	end
 
-	bufmap('<leader>r', vim.lsp.buf.rename)
-	bufmap('<leader>a', vim.lsp.buf.code_action)
+	bufmap('<leader>lr', vim.lsp.buf.rename, '[L]sp [R]ename' )
+	bufmap('<leader>la', vim.lsp.buf.code_action, '[L]sp [A]ction') 
 
-	bufmap('gd', vim.lsp.buf.definition)
-	bufmap('gD', vim.lsp.buf.declaration)
-	bufmap('gI', vim.lsp.buf.implementation)
-	bufmap('<leader>D', vim.lsp.buf.type_definition)
+	bufmap('gd', vim.lsp.buf.definition, '[G]o to [D]efinition')
+	bufmap('gD', vim.lsp.buf.declaration, '[G]o to [D]eclaration')
+	bufmap('gI', vim.lsp.buf.implementation, '[G]o to [I]mplementation')
 
-	bufmap('gr', require('telescope.builtin').lsp_references)
-	bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-	bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+	bufmap('<leader>sr', require('telescope.builtin').lsp_references, '[S]earch [R]eferences')
+	bufmap('<leader>ss', require('telescope.builtin').lsp_document_symbols, '[S]earch [S]ymbols')
+	bufmap('<leader>sS', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[S]earch workspace [S]ymbols')
+	bufmap('<leader>sD', require('telescope.builtin').diagnostics, '[S]earch workspace [D]iagnostics')
+	bufmap('<leader>sd', function() require('telescope.builtin').diagnostics({ bufnr = 0 }) end, '[S]earch [D]iagnostics')
+	bufmap('<leader>se', function() require('telescope.builtin').diagnostics({ bufnr = 0, severity = vim.diagnostic.severity.ERROR }) end, '[S]earch [E]rrors')
 
-	bufmap('K', vim.lsp.buf.hover)
+	bufmap('K', vim.lsp.buf.hover, 'Code Hint')
+
+	-- Show diagnostics in a floating window
+	bufmap('<leader>d', vim.diagnostic.open_float, '[D]iagnostic')
+
+	-- Go to next/previous diagnostic
+	bufmap('[d', vim.diagnostic.goto_prev, 'Previous Diagnostic')
+	bufmap(']d', vim.diagnostic.goto_next, 'Next Diagnostic')
 
 end
 
@@ -26,6 +41,17 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 require('lspconfig').lua_ls.setup {
 	on_attach = on_attach,
 	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }, -- Recognize 'vim' as a global
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false, -- Avoid annoying prompts
+			},
+		},
+	},
 }
 
 require('lspconfig').nil_ls.setup {
--- apps/nvim/plugin/telescope.lua
+++ apps/nvim/plugin/telescope.lua
@@ -13,10 +13,9 @@ require('telescope').setup({
 require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch with [G]rep' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 
 

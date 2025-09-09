e5ab173473b0de734b72cbbddbcd29f26ad1b4b3
WanderingPenwing <wandering.penwing@gmail.com>
2025-09-09 19:10:49 +0200

    NixOS rebuild: try

--- apps/marukuru/install.nix
+++ apps/marukuru/install.nix
@@ -40,11 +40,11 @@ stdenv.mkDerivation rec {
     cp dmenu $out/bin/marukuru
   '';
 
-  # Optional post-fixup to handle dynamic linking
-  postFixup = ''
-    patchelf --add-needed ${xorg.libX11}/lib/libX11.so $out/bin/marukuru
-    patchelf --add-needed ${xorg.libXft}/lib/libXft.so $out/bin/marukuru
-  '';
+  # # Optional post-fixup to handle dynamic linking
+  # postFixup = ''
+  #   patchelf --add-needed ${xorg.libX11}/lib/libX11.so $out/bin/marukuru
+  #   patchelf --add-needed ${xorg.libXft}/lib/libXft.so $out/bin/marukuru
+  # '';
 
   meta = with lib; {
     description = "dmenu";

1e3c439790cbaa79c4895298a48526e44e12315b
WanderingPenwing <wandering.penwing@gmail.com>
2025-09-09 22:14:55 +0200

    NixOS rebuild: try

--- apps/marukuru/install.nix
+++ apps/marukuru/install.nix
@@ -1,28 +1,53 @@
-{ stdenv, lib, fetchFromGitHub}:
+{ stdenv, lib, fetchFromGitHub, autoPatchelfHook, patchelf, xorg, gcc, gnumake, pkg-config }:
 
 stdenv.mkDerivation rec {
   pname = "marukuru";
-  version = "1.1";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
       owner = "WanderingPenwing";
       repo = "Marukuru";
       rev = "${version}";
-      sha256 = "sha256-U4HA0lgBbsgspAR53uBNKle7EUkzGSJ4oTWsaNa5xds=";
+      sha256 = "sha256-u4HA0lgBbsgspAR53uBNKle7EUkzGSJ4oTWsaNa5xds=";
     };
 
+  nativeBuildInputs = [
+    autoPatchelfHook
+    gnumake
+    gcc
+    pkg-config
+  ];
+
+  buildInputs = [
+    xorg.libX11
+    xorg.libXft
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXext
+    xorg.libXpm
+    xorg.libXmu
+  ];
+
+  sourceRoot = "source";
+  
   buildPhase = ''
     make
   '';
 
-  dontFixup = true;
   installPhase = ''
     mkdir -p $out/bin
     cp dmenu $out/bin/marukuru
   '';
 
+  # Optional post-fixup to handle dynamic linking
+  postFixup = ''
+    patchelf --add-needed ${xorg.libX11}/lib/libX11.so $out/bin/marukuru
+    patchelf --add-needed ${xorg.libXft}/lib/libXft.so $out/bin/marukuru
+  '';
+
   meta = with lib; {
-    description = "dmenu-marukuru";
+    description = "dmenu";
     homepage = "https://tools.suckless.org/dmenu/";
     license = licenses.mit;
     platforms = platforms.linux;

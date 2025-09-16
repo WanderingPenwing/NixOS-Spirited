725e469fabe3ea166ec067962b06273694c41332
WanderingPenwing <wandering.penwing@gmail.com>
2025-09-16 16:02:35 +0200

    NixOS rebuild: try

--- apps/jenkins/install.nix
+++ apps/jenkins/install.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "dwm";
-  version = "1.9.9c";
+  version = "1.9.10a";
 
   src = fetchFromGitHub {
     owner = "WanderingPenwing";
     repo = "dwm-Jenkins";
     rev = "${version}";
-    sha256 = "sha256-/a9Oq4Qroob39OuNz4MtJgYAHGQOrZZN69ZuxllcI/4=";
+    sha256 = "sha256-je7OuRZnK07EIUT1cEY/jfh4M57lo+4vihbXEHQaosI=";
   };
 
   nativeBuildInputs = [
